099898b3478a0ffe8067271e16ac4f47442c8a9b
Pedro González Marcos <pedrogonzalezmarcos02@gmail.com>
2026-05-18 12:31:45 +0200

    feat: add sidebar to navigate between changelogs

--- frontend/src/modules/presentation/pages/changelog/index.tsx
+++ frontend/src/modules/presentation/pages/changelog/index.tsx
@@ -7,27 +7,34 @@ export default function ChangelogPage() {
 
   const handleTabChange = (tab: string) => setTab(tab);
 
+  const commonCssRules =
+    'text-lg pl-4 border-l border-transparent hover:border-l-sphere-grey-400 hover:text-black';
+
   return (
-    <section className="px-16">
-      <h1 className="text-3xl mb-4 font-bold">Changelog</h1>
-      <div>
-        <button
-          type="button"
-          className="rounded-md bg-sky-600 px-4 py-2 text-sm font-medium text-white hover:bg-sky-700 disabled:cursor-not-allowed disabled:bg-slate-300"
-          onClick={() => handleTabChange('pricing-intelligence')}
-        >
-          Pricing Intelligence
-        </button>
-        <button
-          type="button"
-          className="rounded-md bg-sky-600 px-4 py-2 text-sm font-medium text-white hover:bg-sky-700 disabled:cursor-not-allowed disabled:bg-slate-300"
-          onClick={() => handleTabChange('sphere')}
-        >
-          SPHERE
-        </button>
+    <section className="px-16 grid grid-cols-[var(--container-2xs)_minmax(0, 1fr)] gap-8">
+      <div className="col_start-1 pt-8 pr-8 border-r border-r-sphere-grey-300">
+        <h2 className="text-lg mb-4 font-semibold">Changelog</h2>
+        <ul className="cursor-pointer border-l border-l-sphere-grey-300">
+          <li onClick={() => handleTabChange('sphere')}>
+            <span
+              className={`${commonCssRules}${tab === 'sphere' ? ' border-l-sphere-grey-600' : ''}`}
+            >
+              Sphere
+            </span>
+          </li>
+          <li onClick={() => handleTabChange('pricing-intelligence')}>
+            <span
+              className={`${commonCssRules}${tab === 'pricing-intelligence' ? ' border-l-sphere-grey-600' : ''}`}
+            >
+              Pricing Intelligence
+            </span>
+          </li>
+        </ul>
       </div>
-      {tab === 'pricing-intelligence' && <PricingIntelligenceChangelog />}
-      {tab === 'sphere' && <StandardChangelog />}
+      <div className="col-start-2">
+        {tab === 'pricing-intelligence' && <PricingIntelligenceChangelog />}
+        {tab === 'sphere' && <StandardChangelog />}
+      </div>
     </section>
   );
 }
